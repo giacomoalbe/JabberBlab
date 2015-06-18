83ab4d44eb9291946ae13d7eee64c0be4788f361
Andrea Brunelli <andrea.brunelli93@gmail.com>
2015-06-18 16:27:00 +0200

    Commit boh, qualche file e penso i database perché c'è scritto derby.log

---
+++ src/java/servlet/ShowUser.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import db.DBManager;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.io.*;
+import static java.lang.System.out;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Andrea
+ */
+public class ShowUser extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        DBManager manager = new DBManager();
+            
+        Connection conn = manager.getConnection();
+        
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from user");
+        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ShowUser</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ShowUser at " + request.getContextPath() + "</h1>");
+            
+            
+            
+             out.println("<table border=1 width=50% height=50%>");
+             out.println("<tr><th>EmpId</th><th>EmpName</th><th>Salary</th><tr>");
+             while (rs.next()) {
+                 String n = rs.getString("username");
+                 String nm = rs.getString("email");
+                 int s = rs.getInt("id_utente"); 
+                 out.println("<tr><td>" + n + "</td><td>" + nm + "</td><td>" + s + "</td></tr>"); 
+             }
+             out.println("</table>");
+             out.println("</html></body>");
+             conn.close();
+            }
+             catch (Exception e) {
+             out.println("error");
+         }
+     }
+ 
+            
+     
+        
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ShowUser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ShowUser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
